76fd9c4c16969f383fe04ddf54eba0cd4c37a2fd
Sébastien Blaise <sebastien.blaise@uclouvain.be>
2016-01-08 20:48:12 +0100

    use a separate file for host name (for using eg a dyndns server)

--- pybox3.py
+++ pybox3.py
@@ -2,6 +2,7 @@
 ### Sebastien Blaise, 2016
 
 ### A file named "userPassword" should be created in the same directory, containing the User bbox3 password
+### A file named "hostName" should be created in the same directory, containing the bbox3 password host name without http://
 
 
 import hashlib
@@ -12,7 +13,7 @@ def login(user, pwd):
     realm = "Technicolor Gateway";
     qop = "auth"
     uri = "/login.lp"
-    initPage = browser.open("http://192.168.1.1/login.lp")
+    initPage = browser.open(host+"/login.lp")
     for line in initPage.read().split('\n'):
         if (line[0:9] == "var nonce"):
             nonce = line[13:-3]
@@ -36,7 +37,7 @@ def antiCSRF(browser, cj):
     browser.form['rn'] = cj[0].value
 
 def reboot(browser, cj):
-    browser.open("http://192.168.1.1/bbox-maintenance.lp")
+    browser.open(host+"/bbox-maintenance.lp")
     browser.select_form(nr = 0)
     antiCSRF(browser, cj)
     browser.form.find_control("main_reboot").readonly = False
@@ -44,7 +45,7 @@ def reboot(browser, cj):
     browser.submit()
 
 def switchVoice1(browser, cj, state):
-    browser.open("http://192.168.1.1/voice-line-1.lp")
+    browser.open(host+"/voice-line-1.lp")
     browser.select_form(name = "voice1")
     antiCSRF(browser, cj)
     browser.form.find_control("voice1_enable").readonly = False
@@ -55,7 +56,7 @@ def switchVoice1(browser, cj, state):
     browser.submit()
 
 def setVoice1Port(browser, cj, port):
-    browser.open("http://192.168.1.1/voice-line-1.lp")
+    browser.open(host+"/voice-line-1.lp")
     browser.select_form(name = "voice1")
     antiCSRF(browser, cj)
     oldPort = browser.form['RSPort1']
@@ -69,7 +70,7 @@ def stripHTML(data):
 
     
 def getIP(browser, cj, index):
-    page = browser.open('http://192.168.1.1/network-global.lp')
+    page = browser.open(host+"/network-global.lp")
     found = 0
     remaining = 0
     for line in page.read().split('\n'):
@@ -93,10 +94,14 @@ def getVoiceIP(browser, cj):
 
 
 user = "User"
-### Insert your User password int the userPassword file
+### Insert your User password in the userPassword file
 with open('userPassword', 'r') as f:
     pwd = f.readline()
+### Insert your bbox3 hostname (without http://) in the userPassword file
+with open('hostName', 'r') as f:
+    host = "http://"+f.readline()
 
+    
 browser = mechanize.Browser()
 browser.set_handle_robots(False)
 browser.addheaders = [("User-agent","Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.2.13) Gecko/20101206 Ubuntu/10.10 (maverick) Firefox/3.6.13")]
